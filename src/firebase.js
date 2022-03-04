1058a809157b27e6f4643521b24838587645240a
hwna00 <layton3534@gmail.com>
2022-03-04 13:27:28 +0000

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZk15gS-WRf6Wg29VIpp38OjgnGLZR0BI",
+  authDomain: "nwitter-clone-17f63.firebaseapp.com",
+  projectId: "nwitter-clone-17f63",
+  storageBucket: "nwitter-clone-17f63.appspot.com",
+  messagingSenderId: "252617133123",
+  appId: "1:252617133123:web:0db8e92ee619d00162d8f6",
+};
+
+export default firebase.initializeApp(firebaseConfig);
