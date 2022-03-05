95eff6e9d328ea6db77d0d3afb19c5b3c1876350
hwna00 <layton3534@gmail.com>
2022-03-05 04:42:11 +0000

    Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomRouter from "components/Router";
 import { authService } from "fb";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <CustomRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <CustomRouter isLoggedIn={isLoggedIn} /> : "Initializing"}
+      <footer>&copy; {new Date().getFullYear()} TwHitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { authService } from "../fb";
+import { authService, firebaseInstance } from "../fb";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [err, setErr] = useState("");
+
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -32,8 +34,26 @@ const Auth = () => {
         console.log(data);
       }
     } catch (error) {
-      console.log(error);
+      setErr(error.message);
     }
+  };
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
 
   return (
@@ -57,12 +77,20 @@ const Auth = () => {
         />
         <input
           type={"submit"}
-          value={newAccount ? "Create Account" : "Log In"}
+          value={newAccount ? "Create Account" : "Sign In"}
         />
+        <div>{err}</div>
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
