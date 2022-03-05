d78480c16f0ff42423a40c8e2aeeae11569b6a0b
hwna00 <layton3534@gmail.com>
2022-03-05 04:59:41 +0000

    Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { authService } from "fb";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <span>Profile</span>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
