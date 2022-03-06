e9158e5d1e47731518590245fc50080968b46920
hwna00 <layton3534@gmail.com>
2022-03-06 08:13:45 +0000

    Get My Own Nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -16,7 +16,7 @@ const CustomRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
           </>
         ) : (
